06846a34ac049033564adf0da9fea353a02a7182
AnalieFS <analieflorezs@gmail.com>
2022-07-13 16:56:27 -0500

    FRONTEND

--- src/src/frontend/src/Components/Inicio.js/CrearNuevo/CrearNuevo.js
+++ src/src/frontend/src/Components/Inicio.js/CrearNuevo/CrearNuevo.js
@@ -1,13 +1,50 @@
 import React, { Component } from "react";
 import "./CrearNuevo.css";
 
-function option() {}
+function tripulation(res) {
+    if(res === "Sí"){
+        return ["Experimentos espaciales", "Incrementar conocimientos"]
+    }
+    else{
+        return["Estudio de cuerpos celestes", "Lanzar carga"]
+    }
+}
+
+function detalleMision(res){
+    if (res === "Experimentos espaciales"){
+        let acciones = [1,2,3,4];
+        let text = "Número de astronautas en la nave"
+        return [acciones, text];
+    }
+    else if (res === "Incrementar conocimientos"){
+        let acciones = [1,2,3];
+        let text = "¿Cuántos exámenes has presentado?"
+        return [acciones, text];
+    }
+    else if(res === "Lanzar carga"){
+        let acciones = ["satelite","Sonda", "Nave"];
+        let text = "Tipo de carga a lanzar"
+        return [acciones, text];
+   }
+    else{
+        let acciones = ["Sí", "No"];
+        let text = "¿Visitará planetas?"
+        return [acciones, text];
+    }
+}
 
 export default class CrearNuevo extends Component {
   
   state = {
     nombre : "",
-  }
+    mision : ["Experimentos espaciales", "Incrementar conocimientos"],
+    acciones : ["a","b"],
+    text : "",
+    showTripulacion: false,
+    showMision : false,
+    showAcciones : false,
+    showText : false
+}
   
     handleName = (e) => {
     let name = e.target.value;
@@ -18,19 +55,32 @@ export default class CrearNuevo extends Component {
         .then((data) => {
             if(data.length===0){
                 this.setState({nombre : name})
+                this.setState({showTripulacion: true})
             }
             else{
                 this.setState({nombre : ""})
+                this.setState({showTripulacion: false})
                 window.alert("Nombre no disponible")
             }
         });
     }
   };
+  handleChange = (e) => {
+    this.setState({mision: tripulation(e.target.value)});
+    console.log(this.state.mision);
+}
+
+handleAction = (e) => {
+    this.setState({acciones : detalleMision(e.target.value)[0]});
+    this.setState({text : detalleMision(e.target.value)[1]});
+}
 
   render() {
     return (
       <div className="CrearNuevoContainer">
+        <div className="FormBanner"></div>
         <div className="FormContainer">
+            <div className="inventarioButton"><h3>Ver inventario</h3></div>
           <h1>ESTACIÓN ESPACIAL SOFKA</h1>
           <h2>¡Bienvenido a la fábrica de creación de naves!</h2>
           <p>
@@ -48,16 +98,30 @@ export default class CrearNuevo extends Component {
               onChange={this.handleName}
               value={this.state.nombre}
             />
-            <label>¿Tu nave lleva tripulación?</label>
-            <select onClick={this.handleChange}>
+            {this.state.showTripulacion&&<label>¿Tu nave lleva tripulación?</label>}
+            {this.state.showTripulacion&&<select onClick={this.handleChange}>
               <option value="Sí">Sí</option>
               <option value="No">No</option>
+            </select>}
+           <label>¿Qué tipo de misión desempeñará?</label>
+            <select onClick={this.handleAction}>
+              {this.state.mision.map(opcion => (
+                <option key={opcion} value={opcion.value}>                                
+                    {opcion}
+                </option>
+              ))}
             </select>
-
+            <label>{this.state.text}</label>
+            <select onClick={this.handleButton}>
+              {this.state.acciones.map(opcion => (
+                <option key={opcion} value={opcion.value}>
+                    {opcion}
+                </option>
+              ))}
+            </select>
             <input type="submit" value="¡Crear nave!"></input>
           </form>
         </div>
-        <div></div>
       </div>
     );
   }
